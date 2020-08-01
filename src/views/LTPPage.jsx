e6b1a20290b9e779410ddcf16999ee75893be8a1
wangzhenyu <wangzhenyu@163.com>
2020-08-01 17:24:57 +0800

    7/31

--- src/views/LTPPage.jsx
+++ src/views/LTPPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import eCharts from 'echarts';
 import {withRouter} from 'react-router-dom';
+import $ from 'jquery';
+
 import {Breadcrumb} from "antd";
 import {HomeOutlined} from '@ant-design/icons';
 
@@ -13,18 +14,28 @@ class LTPPage extends React.Component{
         }
     }
     componentDidMount() {
-        this.state.graph = eCharts.init(document.getElementById('graph'));
+        this.getData();
+        this.setLtp();
+        console.log('width: ' , $('#graph').width() , ', ' , 'height: ' , $('#graph').height());
+    };
 
-        this.state.graph.setOption({
-            title: {
-                show: true,
-                text: 'LTP自然语言处理',
-                textAlign: 'center'
-            },
-            yAxis: {
-                show: false
+    getData(){
+        $.ajax({
+            method: 'POST',
+            url: 'http://39.96.43.154:8080/api',
+            data: {
+                text: '他叫汤姆去拿外衣。'
             }
+        }).then(res => {
+            console.log(res);
         })
+    };
+
+    setLtp(){
+        let graph = document.getElementById('graph').getContext('2d');
+        this.setState({
+            graph
+        });
     };
 
     render(){
@@ -38,7 +49,7 @@ class LTPPage extends React.Component{
                         LTP
                     </Breadcrumb.Item>
                 </Breadcrumb>
-                <div id='graph' style={{width: '100%', height: 800}} />
+                <canvas id='graph' style={{width: '100%', height: 800, border: '1px solid #ccc', boxSizing: 'border-box'}} />
             </div>
         )
     }
